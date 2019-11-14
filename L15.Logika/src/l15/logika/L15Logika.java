c4e6e49068fd8a6586e636cfa51da5f6719ec3fa
cahyasg144 <melvinrtf@gmail.com>
2019-11-14 11:38:11 +0700

    Tugas-Java

---
+++ L15.Logika/src/l15/logika/L15Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l15.logika;
+
+/**
+ * NAMA  : Cahya Satria Gumelar
+ * NIM   : A2.1900028
+ * Kelas : TI-1A
+ * 
+ * @author CahyaSatriaGumelar
+ */
+public class L15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = true;
+       System.out.println("a && b = " + (a && b));
+       System.out.println("a || b = " + (a || b));
+       System.out.println("a !a = " +  !a);
+       System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
